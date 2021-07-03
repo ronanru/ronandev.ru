ddae8d5eff30ee48aa7c73a0d39f6a748437e70f
Ronan <orlition@gmail.com>
2021-07-03 13:19:53 +0300

    glitch effect fix

--- dist/main.js
+++ dist/main.js
@@ -1,7 +1,8 @@
+"use strict";
 const canvas = document.getElementsByTagName('canvas')[0];
 const ctx = canvas.getContext('2d');
 const resetCanvas = () => {
-    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    ctx === null || ctx === void 0 ? void 0 : ctx.clearRect(0, 0, canvas.width, canvas.height);
     const mult = window.innerWidth > 900 ? 1.1 : 1;
     canvas.width = window.innerWidth * mult;
     canvas.height = window.innerHeight * mult;
@@ -9,6 +10,8 @@ const resetCanvas = () => {
 resetCanvas();
 window.onresize = resetCanvas;
 const setPixel = (x, y, color) => {
+    if (!ctx)
+        return;
     ctx.fillStyle = color;
     ctx.fillRect(x, y, 1, 1);
 };
@@ -18,25 +21,21 @@ setInterval(() => {
     setPixel(x, y, '#fafafa');
     setTimeout(() => setPixel(x, y, '#050505'), 15000);
 }, 75);
-window.onmousemove = e => {
+window.onmousemove = (e) => {
     const mouseX = (e.clientX / window.innerWidth) * 5;
     const mouseY = (e.clientY / window.innerHeight) * 5;
     if (window.innerWidth > 900)
         canvas.style.transform = `translate3d(-${mouseX}%, -${mouseY}%, 0)`;
 };
+const p = document.querySelector('.info p');
 setInterval(() => {
     let msg = ["Hi, I'm Ronan", 'I do fun things online'];
-    const initialMsg = msg;
-    for (let ii in msg)
-        for (let i = 0; i < msg[ii].length; i++) {
-            if (Math.random() > 0.99 && msg[ii][i] != ' ') {
+    for (let ii = 0; ii < msg.length; ii++)
+        for (let i = 0; i < msg[ii].length; i++)
+            if (Math.random() > 0.99 && msg[ii][i] != ' ' && msg[ii])
                 msg[ii] = setChar(msg[ii], i, Math.random().toString(36)[10]);
-                setTimeout(() => {
-                    msg[ii] = setChar(msg[ii], i, initialMsg[ii][i]);
-                }, 300);
-            }
-        }
-    document.querySelector('main p').innerHTML = msg.join('<br>');
+    if (p)
+        p.innerHTML = msg.join('<br>');
 }, 100);
 const setChar = (msg, i, char) => msg.substr(0, i) + char + msg.substr(i + 1);
 //# sourceMappingURL=main.js.map
--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,7 @@ const canvas = <HTMLCanvasElement>document.getElementsByTagName('canvas')[0];
 const ctx = canvas.getContext('2d');
 
 const resetCanvas = () => {
-  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  ctx?.clearRect(0, 0, canvas.width, canvas.height);
   const mult = window.innerWidth > 900 ? 1.1 : 1;
   canvas.width = window.innerWidth * mult;
   canvas.height = window.innerHeight * mult;
@@ -13,6 +13,7 @@ resetCanvas();
 window.onresize = resetCanvas;
 
 const setPixel = (x: number, y: number, color: string): void => {
+  if (!ctx) return;
   ctx.fillStyle = color;
   ctx.fillRect(x, y, 1, 1);
 };
@@ -24,26 +25,21 @@ setInterval(() => {
   setTimeout(() => setPixel(x, y, '#050505'), 15000);
 }, 75);
 
-window.onmousemove = e => {
+window.onmousemove = (e: MouseEvent) => {
   const mouseX = (e.clientX / window.innerWidth) * 5;
   const mouseY = (e.clientY / window.innerHeight) * 5;
   if (window.innerWidth > 900) canvas.style.transform = `translate3d(-${mouseX}%, -${mouseY}%, 0)`;
 };
 
 // GLITCH EFFECT
+const p = document.querySelector('.info p');
+
 setInterval(() => {
   let msg = ["Hi, I'm Ronan", 'I do fun things online'];
-  const initialMsg = msg;
-  for (let ii in msg)
-    for (let i = 0; i < msg[ii].length; i++) {
-      if (Math.random() > 0.99 && msg[ii][i] != ' ') {
-        msg[ii] = setChar(msg[ii], i, Math.random().toString(36)[10]);
-        setTimeout(() => {
-          msg[ii] = setChar(msg[ii], i, initialMsg[ii][i]);
-        }, 300);
-      }
-    }
-  document.querySelector('main p').innerHTML = msg.join('<br>');
+  for (let ii = 0; ii < msg.length; ii++)
+    for (let i = 0; i < msg[ii].length; i++) if (Math.random() > 0.99 && msg[ii][i] != ' ' && msg[ii]) msg[ii] = setChar(msg[ii], i, Math.random().toString(36)[10]);
+
+  if (p) p.innerHTML = msg.join('<br>');
 }, 100);
 
 const setChar = (msg: string, i: number, char: string): string => msg.substr(0, i) + char + msg.substr(i + 1);
